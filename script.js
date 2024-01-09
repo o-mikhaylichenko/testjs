18496c51a83f072bebbc51e621565983bc567431
o-mikhaylichenko <103101603+o-mikhaylichenko@users.noreply.github.com>
2024-01-09 21:47:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-console.log('Hello World');
-let userName = prompt('Как Вас зовут?');
-alert(`Привет ${userName}`);
+let a = +prompt('Введите первое число');
+let b = +prompt('Введите второе число');
+
+let min = a;
+
+if (b < min) {
+  min = b;  
+};
+
+if (a === b) {
+    console.log(`Число ${a} и число ${b} равны`); 
+} else {
+    console.log(`Минимальное число ${min}`);
+}
+
